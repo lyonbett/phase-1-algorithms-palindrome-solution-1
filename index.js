23dfb533ebf7168ad4217646aa085b5e346338f1
lyonbett <lyon.bett@student.moringaschool.com>
2023-04-10 03:18:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word){
+  const wordArr = word.split("")
+  const reverseWordArr = wordArr.reverse()
+  const reversedWord = reverseWordArr.join("")
+
+  return reversedWord
 }
 
-/* 
-  Add your pseudocode here
-*/
+function isPalindrome(word) {
+  // Write your algorithm here
+  //reverse the input string
+  const reverseWord = reverse(word)
+  // If the input is the same as the reversed input
+  if (word === reverseWord){
+    return true;
+  }else{
+    return false;
+  }
+}
 
 /*
   Add written explanation of your solution here
+
+  this code is a simple and efficient solution to check whether a string is a palindrome or not.
 */
 
 // You can run `node index.js` to view these console logs
